bb90a82976e81aded65feb227006969386cc071b
AayushPandeyy <ap200061@gmail.com>
2024-05-06 17:01:52 +0545

    Updates

--- frontend/src/RouterComponent.jsx
+++ frontend/src/RouterComponent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginComponent from "./components/auth/LoginComponent.jsx";
 import Dashboard from "./components/middleware/AdminRouteMiddleware.jsx";
+import DashboardComponent from "./components/admin/DashboardComponent.jsx";
 
 export default function RouterComponent() {
   return (
@@ -11,7 +12,12 @@ export default function RouterComponent() {
           path="/login"
           element={<LoginComponent></LoginComponent>}
         ></Route>
-        <Route path="/admin" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/admin" element={<Dashboard></Dashboard>}>
+          <Route
+            path="/admin"
+            element={<DashboardComponent></DashboardComponent>}
+          ></Route>
+        </Route>
       </Routes>
     </div>
   );
--- frontend/src/components/middleware/AdminRouteMiddleware.jsx
+++ frontend/src/components/middleware/AdminRouteMiddleware.jsx
@@ -22,7 +22,6 @@ export default function Dashboard() {
         } else {
           setIsLogin(false);
           setIsLoading(false);
-          
         }
       })
       .catch((error) => {});
